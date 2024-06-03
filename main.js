6aa46b36961f9bf16e4427d520cf938b9337893c
Shumimran <164326671+Shumimran@users.noreply.github.com>
2024-06-03 12:29:47 +0500

    Add files via upload
    Aouther: shumaila imran

---
+++ main.js
@@ -0,0 +1,6 @@
+"use strict";
+// Double Numbers in an Array: Make a list of numbers. Then, use a trick to make a new list where each number 
+// is twice its original value.
+let numbers = [1, 2, 3, 4, 5];
+let DoubleNumbers = numbers.map(number => number * 2);
+console.log(DoubleNumbers);
